ce8cc71299d7e869e3548a79b979b696cd4b5c2f
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-05-28 09:30:33 +0900

    5.1.3：カスタムトランジションクラスの追加

--- chapter-5/5.1.3/app.js
+++ chapter-5/5.1.3/app.js
@@ -2,7 +2,14 @@ new Vue({
     el: '#app',
     data: function () {
         return {
+            animationClass: "bounce",
             isShown: false
+        }
+    },
+    computed: {
+        activeClass: function() {
+            // 設定するクラスの値を計算する。インプットフィールドの入力に応じて再計算される
+            return this.animationClass + ' animated'
         }
     }
 })
