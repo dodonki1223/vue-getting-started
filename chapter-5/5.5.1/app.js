180b80bb6e3039971bc7d305d7f77fc9d76f7c85
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-06-03 10:08:38 +0900

    5.5.1：ミックスインで機能を再利用する-2

--- chapter-5/5.5.1/app.js
+++ chapter-5/5.5.1/app.js
@@ -1,18 +1,16 @@
-var IconShareButton = {
-  template: `
-    <button @click="share"><i class="fas fa-share-square"></i></button>
-  `,
+// ミックスインの定義
+var Sharable = {
   data: function() {
     return {
       _isProcessing: false
     }
   },
+  created: function() {
+    console.log('Sharableミックスインのフックが呼ばれました')
+  },
   methods: {
     share: function() {
       if (this._isProcessing) {
-        return
-      }
-      if (!window.confirm('シェアしますか？')) {
         return
       }
       this._isProcessing = true
@@ -25,30 +23,33 @@ var IconShareButton = {
   }
 }
 
+var IconShareButton = {
+  mixins: [Sharable],
+  created: function() {
+    console.log('IconShareButtonミックスインのフックが呼ばれました')
+  },
+  template: `
+    <button @click="share"><i class="fas fa-share-square"></i></button>
+  `
+}
+
 var TextShareButton = {
+  mixins: [Sharable],
+  created: function() {
+    console.log('TextShareButtonミックスインのフックが呼ばれました')
+  },
   template: `
     <button @click="share">{{ buttonLabel }}</button>
-  `,
+  `,  
   data: function() {
     return {
-      buttonLabel: 'シェアする',
-      _isProcessing: false
+      buttonLabel: 'シェアする'
     }
   },
   methods: {
-    share: function() {
-      if(this._isProcessing){
-        return
-      }
-      if(!window.confirm('シェアしますか？')){
-        return
-      }
-      this._isProcessing = true
-      // 実際はここでSNSのSDKのAPIを呼び出す
-      setTimeout(() => {
-        window.alert('シェアしました')
-        this._isProcessing = false
-      }, 300)
+    share() {
+      // どちらのメソッドがよばれる？
+      window.alert('コンポーネントからシェアしました')
     }
   }
 }
