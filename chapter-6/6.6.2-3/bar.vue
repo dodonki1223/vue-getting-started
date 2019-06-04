460617b33ee7811e7166b3ff0c6f17c07b42a9ba
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-06-04 10:55:12 +0900

    6.6.2：スコープ付きCSS - 複数の単一ファイルのコンポーネントにおいてscoped属性でカプセル化されたスタイル

---
+++ chapter-6/6.6.2-3/bar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bar">
+    <h1 class="header">Barコンポーネント</h1>
+    <p>これはBarコンポーネントです。</p>
+    <foo class="header" /> <!-- Fooコンポーネントの利用。scriptブロックも要注意 -->
+  </div>
+</template>
+
+<script>
+import Foo from './foo'
+export default { //FooコンポーネントをBarコンポーネントに登録
+  components: {
+    Foo
+  }
+}
+</script>
+
+<style scoped>
+.bar {
+  border: 1px solid red;
+  margin: 4px;
+  padding: 4px;
+}
+.header { font-size: 125%; }
+</style>
