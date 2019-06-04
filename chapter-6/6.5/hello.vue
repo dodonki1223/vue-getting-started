9b64df3ad11adee945f26d2a6f351bb1efde6300
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-06-04 09:45:40 +0900

    6.5：単一ファイルコンポーネントの動作を体験する

---
+++ chapter-6/6.5/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="message">メッセージ：{{ msg }}</p>
+</template>
+
+<script>
+export default {
+  data () {
+    return { msg: 'こんにちわ！' }
+  }
+}
+</script>
+
+<style>
+.message { color: #42b983; }
+</style>
