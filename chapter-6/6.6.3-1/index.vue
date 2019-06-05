644e1fc76a302b9422162bfed6d85cd33bd569a3
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-06-05 10:23:32 +0900

    6.6.3：CSSモジュール - 名前付きスタイル識別子の算出プロパティ

---
+++ chapter-6/6.6.3-1/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <form novalidate>
+    <p :class="alertValidation">{{ validMessage }}</p> 
+    <textarea @input="onInput" :class="textboxValidation"></textarea>
+  </form>
+</template>
+<script>
+export default {
+  data () {
+    return { valid: false }
+  },
+  computed: {
+    validMessage () {
+      return this.valid ? '入力されています。':'入力されていません。'
+    },
+    alertValidation () {
+      return this.valid ? this.alert.success : this.alert.error
+    },
+    textboxValidation () {
+      return this.valid ? this.textbox.success : this.textbox.error
+    }
+  },
+  methods: {
+    isRequired (value) {
+      return value.length > 0
+    },
+    onInput (e) {
+      this.valid = this.isRequired(e.target.value)
+    }
+  }
+}
+</script>
+
+<style module="alert">
+.success {
+  color: green;
+}
+.error {
+  font-weight: bold;
+  color: red;
+}
+</style>
+
+<style module="textbox">
+.success {
+  border: solid 2px green;
+}
+.error {
+  border: solid 2px red;
+}
+</style>
