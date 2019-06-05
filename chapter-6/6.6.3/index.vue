e7b622d34021a3375fa05363a4023e5eb363e2c1
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-06-05 09:35:55 +0900

    6.6.3：CSSモジュール - スタイルのモジュール化を実現する仕組み

---
+++ chapter-6/6.6.3/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <p :class="$style.message">こんにちは！</p>
+</template>
+<script>
+export default {
+  created() {
+    // eslint-disable-next-line no-console
+    console.log('css modules: $style', this.$style)
+  }
+}
+</script>
+
+<style module>
+.message { color: #42b983 }
+</style>
