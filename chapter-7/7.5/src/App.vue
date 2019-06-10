ef19abb9de1411c379b51c64a91322ba805b315c
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-06-10 10:08:17 +0900

    7.5.2：タスクの一覧表示

--- chapter-7/7.5/src/App.vue
+++ chapter-7/7.5/src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app">
-    <!-- ここに実装を書いていく -->
+    <h2>タスク一覧</h2>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id">
+        <input type="checkbox" v-bind:checked="task.done">
+        {{ task.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  // ここに実装を書いていく
+  computed: {
+    tasks () {
+      return this.$store.state.tasks // ストアを読む
+    },
+  },
 }
 </script>
--- chapter-7/7.5/src/store.js
+++ chapter-7/7.5/src/store.js
@@ -5,7 +5,21 @@ Vue.use(Vuex)
 
 // ストアの定義
 const store = new Vuex.Store({
-  // ここに実装を書いていく
+  state: {
+    // タスクの初期ステート
+    tasks: [
+      {
+        id: 1,
+        name: '牛乳を買う',
+        done: false
+      },
+      {
+        id: 2,
+        name: 'Vue.jsの本を買う',
+        done: true
+      }
+    ],
+  },
 })
 
 // ストアをエクスポート
