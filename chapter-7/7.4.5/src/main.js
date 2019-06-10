96aecb027749310ae045bf1528198a19cdc8305e
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-06-10 09:41:22 +0900

    7.4.5：アクション - 2

--- chapter-7/7.4.5/src/main.js
+++ chapter-7/7.4.5/src/main.js
@@ -3,25 +3,60 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// 例示用に非同期処理を行う関数
+// 実際のアプリではサーバーからデータを取得する
+function getCountNum (type) {
+  return new Promise(resolve => {
+    // 1秒後にtypeに応じたデータを返す
+    setTimeout(() => {
+      let amount
+      switch (type) {
+        case 'one':
+          amount = 1
+          break
+        case 'two':
+          amount = 2
+          break
+        case 'ten':
+          amount = 10
+          break
+        default:
+          amount = 0
+      }
+      resolve({ amount })
+    }, 1000)
+  })
+}
+
 const store = new Vuex.Store({
   state: {
     count: 10
   },
   mutations: {
-    increment (state) {
-      state.count = state.count + 1
+    increment (state, payload) {
+      state.count += payload.amount
     }
   },
   // actionオプションでアクションを定義する
   actions: {
-    incrementAction (ctx) {
-      // `increment`ミューテーションを実行する
-      ctx.commit('increment')
+    incrementAsync ({ commit }, payload) {
+      return getCountNum(payload.type)
+
+        .then(data => {
+          // レスポンスをログに表示
+          console.log(data)
+
+          // レスポンスをペイロードとして渡したミューテーションを実行する
+          commit('increment', {
+            amount: data.amount
+          })
+        })
     }
   }
 })
 
-// store.dispatchでアクションを呼び出す
-console.log(store.state.count)    // -> 10
-store.dispatch('incrementAction') // `incrementAction` アクションを呼び出す
-console.log(store.state.count)    // -> 11
+console.log(store.state.count) // -> 10
+store.dispatch('incrementAsync', { type: 'one' }).then(() => {
+  // アクションの処理が完了した後に実行される
+  console.log(store.state.count) // -> 11
+})
