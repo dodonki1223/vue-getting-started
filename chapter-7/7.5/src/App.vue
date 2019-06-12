b6a0c124a93ff5aa746a4b09f27c6caaf73e3462
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-06-12 09:50:57 +0900

    7.5.3：タスクの新規作成と完了

--- chapter-7/7.5/src/App.vue
+++ chapter-7/7.5/src/App.vue
@@ -3,19 +3,46 @@
     <h2>タスク一覧</h2>
     <ul>
       <li v-for="task in tasks" v-bind:key="task.id">
-        <input type="checkbox" v-bind:checked="task.done">
+        <input type="checkbox" v-bind:checked="task.done"
+         v-on:change="toggleTaskStatus(task)">
         {{ task.name }}
       </li>
     </ul>
+
+    <form v-on:submit.prevent="addTask">
+      <input type="text" v-model="newTaskName" placeholder="新しいタスク">
+    </form>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 入力中の新しいタスク名を一時的に保持する
+      newTaskName: '',
+    }
+  },
   computed: {
     tasks () {
       return this.$store.state.tasks // ストアを読む
     },
   },
+  methods: {
+    // タスクを追加する
+    addTask() {
+      // `addTask`ミューテーションをコミット
+      $this.$store.commit('addTask', {
+        name: this.newTaskName,
+      })
+      this.newTaskName = ''
+    },
+    // タスクの完了状態を更新する
+    toggleTaskStatus (task) {
+      this.$store.commit('toggleTaskStatus', {
+        id: task.id
+      })
+    },
+  }
 }
 </script>
--- chapter-7/7.5/src/store.js
+++ chapter-7/7.5/src/store.js
@@ -19,7 +19,31 @@ const store = new Vuex.Store({
         done: true
       }
     ],
+    // 次に追加するタスクのID
+    // 実際のアプリではサーバーで生成したり、UUIDを使ったりするがここでは決め打ち
+    nextTaskId: 3,
   },
+  mutations: {
+    // タスクを追加する
+    addTask (state, { name }) {
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        done: false
+      })
+      // 次に追加されるタスクに付与するIDを更新する
+      state.nextTaskId++
+    },
+    // タスクの完了状態を変更する
+    toggleTaskStatus (state, { id }) {
+      const filterd = state.tasks.filter(task => {
+        return task.id === id
+      })
+      filterd.forEach(task => {
+        task.done = !task.done
+      })
+    },
+  }
 })
 
 // ストアをエクスポート
