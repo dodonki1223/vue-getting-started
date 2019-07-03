d633590894fc708f4bb0d0bf647780458c8bcf73
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-07-03 10:25:13 +0900

    8.8.5：状態管理ライブラリの導入

--- kanban-app/src/main.js
+++ kanban-app/src/main.js
@@ -1,15 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'es6-promise/auto' // プロミスをポリフィルする
 import App from './App'
 import router from './router'
+import store from './store' // Vuexのストアインスタンスをインポート
 
-Vue.config.productionTip = false
+Vue.config.productionTip = process.env.NODE_ENV === 'production'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store, // インポートしたストアインスタンスを`store`オプションとして指定
+  render: h => h(App)
 })
