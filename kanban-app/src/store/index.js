8a027436ac1f826b050683ff5b39d6dea431831f
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-07-10 09:08:41 +0900

    9.3.1：状態モデリング

--- kanban-app/src/store/index.js
+++ kanban-app/src/store/index.js
@@ -6,7 +6,19 @@ import mutations from './mutations'
 
 Vue.use(Vuex)
 
+// 状態`Auth`と状態`Board`をVuexのstateで一元管理出来るよう定義する
+const state = {
+  auth: {        // 状態`Auth`
+    token: null, // `token`はnullで初期化
+    userId: null // `userId`はnullで初期化
+  },
+  board: {    // 状態`Board`
+    lists: [] // 状態`TaskList`は空で初期化
+  }
+}
+
 export default new Vuex.Store({
+  state, // 定義したstateを`state`オプションに指定
   getters,
   actions,
   mutations,
