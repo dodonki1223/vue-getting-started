183f02f1424a7683f768cba5d1429600da6767d1
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-07-16 08:59:36 +0900

    9.3.3：データフロー周りの雛形コードのセットアップ

---
+++ kanban-app/src/store/mutations.js
@@ -0,0 +1,28 @@
+import * as types from './mutation-types'
+
+export default {
+  [types.AUTH_LOGIN] (state, payload) {
+    // TODO:
+    throw new Error('AUTH_LOGIN mutation should be implemented')
+  },
+  [types.FETCH_ALL_TASKLIST] (state, payload) {
+    // TODO:
+    throw new Error('FETCH_ALL_TASKLIST mutation should be implemented')
+  },
+  [types.ADD_TASK] (state, payload) {
+    // TODO:
+    throw new Error('ADD_TASK mutation should be implemented')
+  },
+  [types.UPDATE_TASK] (state, payload) {
+    // TODO:
+    throw new Error('UPDATE_TASK mutation should be implemented')
+  },
+  [types.REMOVE_TASK] (state, payload) {
+    // TODO:
+    throw new Error('REMOVE_TASK mutation should be implemented')
+  },
+  [types.AUTH_LOGOUT] (state, payload) {
+    // TODO:
+    throw new Error('AUTH_LOGOUT mutation should be implemented')
+  }
+}
