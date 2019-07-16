1963ed094c8d44ca3ae5bb77e13e0b783065725b
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-07-16 09:18:13 +0900

    9.3.4：アクションの雛形

---
+++ kanban-app/src/store/actions.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import * as types from './mutation-types'
+import { Auth, List, Task } from '../api'
+/* eslint-enable no-unused-vars */
+
+export default {
+  login: ({ commit }) => {
+    // TODO:
+    throw new Error('login action should be implemented')
+  },
+
+  fetchLists: ({ commit }) => {
+    // TODO:
+    throw new Error('fetchLists action should be implemented')
+  },
+
+  addTask: ({ commit }) => {
+    // TODO:
+    throw new Error('addTask action should be implemented')
+  },
+
+  updateTask: ({ commit }) => {
+    // TODO:
+    throw new Error('updateTask action should be implemented')
+  },
+
+  removeTask: ({ commit }) => {
+    // TODO:
+    throw new Error('removeTask action should be implemented')
+  },
+
+  logout: ({ commit }) => {
+    // TODO:
+    throw new Error('logout action should be implemented')
+  },
+}
