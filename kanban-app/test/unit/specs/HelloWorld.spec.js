aca72f9632ad977b5e410faeb7dee5295421a50e
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-08-30 08:32:59 +0900

    10.1.1：アプリケーションの実装に入る前に

--- kanban-app/test/unit/specs/HelloWorld.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld'
-
-describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
-    expect(mount(HelloWorld).find('.hello h1').text())
-      .to.equal('Welcome to Your Vue.js App')
-  })
-})
