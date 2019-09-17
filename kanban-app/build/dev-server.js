f5d1c8737a097ed50ae3e3c17a520bd69548aa39
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-09-17 10:04:45 +0900

    10.5.1：開発サーバーによる開発

--- kanban-app/build/dev-server.js
+++ kanban-app/build/dev-server.js
@@ -6,5 +6,27 @@ module.exports = app => {
   // HTTPリクエストのbodyの内容をJSONとして解析するようミドルウェアをインストール
   app.use(bodyParser.json())
 
-  // TODO:ここ行こうにAPIの実装内容を追加していく
+  // ユーザー情報
+  const users = {
+    'foo@domain.com': {
+      password: '12345678',
+      userId: 1,
+      token: '1234567890abcdef'
+    }
+  }
+
+  // ログインAPIのエンドポイント'/auth/login'
+  app.post('/auth/login', (req, res) => {
+    const { email, password } = req.body
+    const user = users[email]
+    if (user) {
+      if (user.password !== password) {
+        res.status(401).json({ message: 'ログインに失敗しました。' })
+      } else {
+        res.json({ userId: user.userId, token: user.token })
+      }
+    } else {
+      res.status(404).json({ message: 'ユーザーが登録されていません。' })
+    }
+  })
 }
