b07802c922ccd67980fe572d87914096c0537e7a
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-10-02 10:15:57 +0900

    C.4.6：静的なHTMLファイルにビルドする

--- nuxt-static-example/pages/Product.vue
+++ nuxt-static-example/pages/Product.vue
@@ -1,3 +1,18 @@
 <template>
-  <h2>このページはNuxt社のトップのページです。</h2>
+  <h2>{{ title }}</h2>
 </template>
+
+<script>
+export default {
+  async asyncData (context) {
+    return await new Promise(resolve => {
+      // バックエンドからのデータ取得をエミュレート
+      setTimeout(() => {
+        resolve({
+          title: `このページはNuxt社のプロダクト情報のページです。(${ context.isServer ? 'サーバーサイドレンダリング': 'クライアントサイドレンダリング'})`
+        })
+      }, 1000)
+    })
+  }
+}
+</script>
